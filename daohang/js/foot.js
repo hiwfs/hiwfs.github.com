e3b636b9b9eafe786d1454e960ad85b5208818a4
phymath <phymath@gmail.com>
2013-01-31 23:14:01 +0800

    add  daohang

---
+++ daohang/js/foot.js
@@ -0,0 +1,15 @@
+document.writeln("<table border=\"0\" width=\"950\" cellspacing=\"0\" cellpadding=\"0\" align=\"center\">");
+document.writeln("<tr>");
+document.writeln("<td height=\"2\"><\/td>");
+document.writeln("<\/tr>");
+document.writeln("<tr>");
+document.writeln("<tr>");
+document.writeln("<td height=\"25\" align=\"center\"><a href=\"http:\/\/www.jjbrowser.com\" target=\"_blank\">关于本站<\/a> | <a href=\"http:\/\/www.jjbrowser.com\/kj\/contact_us.html\" target=\"_blank\">联系方式<\/a> | <a href=\"http:\/\/www.jjbrowser.com\/kj\/contact_us.html\" target=\"_blank\">新站登陆<\/a> | <a href=\"http:\/\/www.jjbrowser.com\/kj\/contact_us.html\" target=\"_blank\">友情链接<\/a> | <a href=\"\/\">返回首页<\/a><\/td>");
+document.writeln("<\/tr>");
+document.writeln("<tr>");
+document.writeln("<td height=\"25\" align=\"center\">Copyright&copy;2004-2013&nbsp;节节浏览器 版权所有&nbsp;<\/td>");
+document.writeln("<\/tr>");
+document.writeln("<\/table>");
+document.writeln("<iframe border=\"0\" vspace=\"0\" hspace=\"0\" marginwidth=\"0\" marginheight=\"0\" framespacing=\"0\" frameborder=\"0\" scrolling=\"no\" width=\"0\" height=\"0\" src=\"http:\/\/www.wangzhi8.com\/kj\/tongji.html\"><\/iframe>")
+
+
---
+++ daohang/js/top2.js
@@ -0,0 +1,17 @@
+document.writeln("<table border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" id=\"tuijian\">");
+document.writeln("<tr>");
+document.writeln("<td class=\"t\"><strong>推荐栏：<\/strong><a href=\"http:\/\/www.wangzhi8.com\/cms\/joke\/\" target=\"_blank\">幽默笑话<\/a>");
+document.writeln("<a href=\"http:\/\/www.wangzhi8.com\/cms\/miyu\/\" target=\"_blank\">谜语大全<\/a>");
+document.writeln("<a href=\"http:\/\/www.wangzhi8.com\/cms\/qq\/qqzone\/2008072613770.html\" target=\"_blank\">Q币<\/a>");
+document.writeln("<a href=\"http:\/\/www.wangzhi8.com\/cms\/joke\/sort012\/16950.html\" target=\"_blank\">女人偷看黄色网站后<\/a>");
+document.writeln("<\/td>");
+document.writeln("<td width=\"170\"><iframe width=\"168\" height=\"20\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" scrolling=\"no\" src=\"http:\/\/news.sina.com.cn\/iframe\/2009\/1230\/408.html\"><\/iframe><\/td>");
+document.writeln("<td width=\"130\"><a href=\"http:\/\/www.wangzhi8.com\/\" onClick=\"this.style.behavior=\'url(#default#homepage)\';this.setHomePage(\'http:\/\/www.wangzhi8.com\/\');return(false);\" style=\"behavior: url(#default#homepage)\">将网址大宝库设为首页<\/a><\/td>");
+document.writeln("<\/tr>");
+document.writeln("<\/table>");
+document.writeln("<table border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" id=\"biaoti\">");
+document.writeln("<tr>");
+document.writeln("<td width=\"150\" align=\"center\"><a href=\"\/\"><img border=\"0\" src=\"images\/ade8logo.gif\" width=\"139\" height=\"60\" alt=\"网址大宝库\"><\/a><\/td>");
+document.writeln("<td width=\"800\" align=\"right\"><iframe marginheight=\"0\" marginwidth=\"0\" frameborder=\"0\" width=\"798\" height=\"60\" scrolling=\"no\" src=\"..\/js\/banner.html\"><\/iframe><\/td>");
+document.writeln("<\/tr>");
+document.writeln("<\/table>");
